3eb79f65e228d895266fb0f6e8a0af703417a230
Warren Goh <warrengohkscode@gmail.com>
2015-05-06 12:10:36 -0600

    RightBar Button Implemented

--- NavigationColors/ViewController.m
+++ NavigationColors/ViewController.m
@@ -17,11 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //setup barbuton, perfect place to set this up
+    UIBarButtonItem *colorButton = [[UIBarButtonItem alloc]initWithTitle:@"color" style:UIBarButtonItemStylePlain target:self action:@selector(color:)];
+    //colon is part of the name!!!!!!
+    self.navigationItem.rightBarButtonItem = colorButton;
+    //when this view controller is on the screen, it will want a right bar button of this color
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+//creathe method for sction
+
+-(IBAction)color:(id)sender{
+  if (self.view.backgroundColor == [UIColor blueColor]){
+      self.view.backgroundColor = [UIColor whiteColor];
+                                   } else {
+                                         self.view.backgroundColor = [UIColor blueColor];
+                                     }
+    
 }
 
 @end
